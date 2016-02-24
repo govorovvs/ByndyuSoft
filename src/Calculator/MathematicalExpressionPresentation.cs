56f673846257eaaf5fc9d86a07403ea142a647c7
unknown <victor@govorov-laptop.govorov.ru>
2016-02-24 22:47:38 +0500

    added MathematicalExpressionPresentation->addValue

--- src/Calculator/MathematicalExpressionPresentation.cs
+++ src/Calculator/MathematicalExpressionPresentation.cs
@@ -1,7 +1,30 @@
-﻿namespace Calculator
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Calculator
 {
-	public class MathematicalExpressionPresentation
+	public class MathematicalExpressionPresentation : IReadOnlyCollection<MathematicalExpressionPresentationItem>
 	{
-		 
+		private readonly List<MathematicalExpressionPresentationItem> _items = new List<MathematicalExpressionPresentationItem>();
+
+		public IEnumerator<MathematicalExpressionPresentationItem> GetEnumerator()
+		{
+			return _items.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public int Count
+		{
+			get { return _items.Count; }
+		}
+
+		public void AddValue(decimal value)
+		{
+			_items.Add(new MathematicalExpressionPresentationValueItem(value));
+		}
 	}
 }
---
+++ tests/Calculator.Tests.Unit/MathematicalExpressionPresentationTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Tests.Unit
+{
+	[TestFixture]
+	public class MathematicalExpressionPresentationTests
+	{
+		private MathematicalExpressionPresentation _presentation;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_presentation = new MathematicalExpressionPresentation();
+		}
+
+		[Test]
+		public void TestAddValue()
+		{
+			// arrange
+			const decimal value = 10;
+
+			// act
+			_presentation.AddValue(value);
+
+			// assert
+			CollectionAssert.AreEquivalent(
+				new[] {new MathematicalExpressionPresentationValueItem(value)}, _presentation);
+		}
+	}
+}
