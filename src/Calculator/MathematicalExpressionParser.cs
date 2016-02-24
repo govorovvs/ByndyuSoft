7d878481847c6490935d2df4f883bcc558cb3944
unknown <victor@govorov-laptop.govorov.ru>
2016-02-24 23:59:32 +0500

    added MathematicalExpressionParserTests->ParseAddition

--- src/Calculator/MathematicalExpressionParser.cs
+++ src/Calculator/MathematicalExpressionParser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using Calculator.Operations;
 
 namespace Calculator
 {
@@ -11,12 +12,66 @@ namespace Calculator
 			if (expression == null) throw new ArgumentNullException(nameof(expression));
 
 			var output = new List<object>();
+			var operationsStack = new Stack<IArithmeticOperation>();
 
-			decimal value = decimal.Parse(expression, CultureInfo.InvariantCulture);
+			string valueString = string.Empty;
 
-			output.Add(value);
+			for (int i = 0; i <= expression.Length; i++)
+			{
+				bool isTheEndOfString = i == expression.Length;
+				if (isTheEndOfString)
+				{
+					ProcessValue(ref valueString, output);
+					break;
+				}
 
+				char currentSymbol = expression[i];
+
+				IArithmeticOperation operation;
+				if (TryGetOperation(currentSymbol, out operation))
+				{
+					ProcessOperation(ref valueString, operation, operationsStack, output);
+					continue;
+				}
+
+				valueString += currentSymbol;
+			}
+
+			MoveOperationsFromStackToOutput(operationsStack, output);
 			return MathematicalExpressionPresentation.Create(output);
+		}
+
+		private void ProcessValue(ref string valueString, List<object> output)
+		{
+			decimal value = decimal.Parse(valueString, CultureInfo.InvariantCulture);
+			output.Add(value);
+			valueString = string.Empty;
+		}
+
+		private void ProcessOperation(ref string valueString, IArithmeticOperation operation, Stack<IArithmeticOperation> operationsStack, List<object> output)
+		{
+            ProcessValue(ref valueString, output);
+			operationsStack.Push(operation);
+		}
+
+		private bool TryGetOperation(char currentSymbol, out IArithmeticOperation operation)
+		{
+			if (currentSymbol == '+')
+			{
+				operation = Addition.Instance;
+				return true;
+			}
+
+			operation = null;
+			return false;
+		}
+
+		private void MoveOperationsFromStackToOutput(Stack<IArithmeticOperation> operationsStack, List<object> output)
+		{
+			while (operationsStack.Count != 0)
+			{
+				output.Add(operationsStack.Pop());
+			}
 		}
 	}
 }
--- tests/Calculator.Tests.Unit/MathematicalExpressionParserTests.cs
+++ tests/Calculator.Tests.Unit/MathematicalExpressionParserTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Calculator.Operations;
+using NUnit.Framework;
 
 namespace Calculator.Tests.Unit
 {
@@ -52,6 +53,20 @@ namespace Calculator.Tests.Unit
 
 			// assert
 			var expected = MathematicalExpressionPresentation.Create(12.3m);
+			CollectionAssert.AreEquivalent(expected, result);
+		}
+
+		[Test]
+		public void ParseAddition()
+		{
+			// arrange
+			const string expression = "1+2";
+
+			// act
+			var result = _parser.Parse(expression);
+
+			// assert
+			var expected = MathematicalExpressionPresentation.Create(1,2,Addition.Instance);
 			CollectionAssert.AreEquivalent(expected, result);
 		}
 	}
