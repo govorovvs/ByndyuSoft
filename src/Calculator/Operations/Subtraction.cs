947d105d65b40041563f7eef887d62e66ced2ef9
unknown <victor@govorov-laptop.govorov.ru>
2016-02-24 22:28:32 +0500

    added Subtraction operation

---
+++ src/Calculator/Operations/Subtraction.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculator.Operations
+{
+	public class Subtraction : IArithmeticOperation
+	{
+		public static Subtraction Instance = new Subtraction();
+
+		private Subtraction()
+		{
+			Symbol = '-';
+			Priority = 2;
+		}
+
+		public char Symbol { get; private set; }
+
+		public int Priority { get; private set; }
+
+		public decimal Execute(decimal first, decimal second)
+		{
+			return first - second;
+		}
+	}
+}
---
+++ tests/Calculator.Tests.Unit/Operations/SubtractionTests.cs
@@ -0,0 +1,41 @@
+﻿using Calculator.Operations;
+using NUnit.Framework;
+
+namespace Calculator.Tests.Unit.Operations
+{
+	[TestFixture]
+	public class SubtractionTests
+	{
+		private Subtraction _operation;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_operation = Subtraction.Instance;
+		}
+
+		[Test]
+		public void HasSubtractionSymbol()
+		{
+			// assert
+			Assert.AreEqual('-', _operation.Symbol);
+		}
+
+		[Test]
+		public void HasTwoPriority()
+		{
+			// assert
+			Assert.AreEqual(2, _operation.Priority);
+		}
+
+		[Test]
+		public void TestExecute()
+		{
+			// act
+			decimal result = _operation.Execute(10, 20);
+
+			// assert
+			Assert.AreEqual(-10, result);
+		}
+	}
+}
