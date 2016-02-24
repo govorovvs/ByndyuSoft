52c28cb81ed7d7d17a83d72e08c641d7f1af66a6
unknown <victor@govorov-laptop.govorov.ru>
2016-02-24 22:33:20 +0500

    added Division operation

---
+++ src/Calculator/Operations/Division.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculator.Operations
+{
+	public class Division : IArithmeticOperation
+	{
+		public static readonly Division Instance = new Division();
+
+		private Division()
+		{
+			Symbol = '/';
+			Priority = 3;
+		}
+
+		public char Symbol { get; private set; }
+
+		public int Priority { get; private set; }
+
+		public decimal Execute(decimal first, decimal second)
+		{
+			return first / second;
+		}
+	}
+}
---
+++ tests/Calculator.Tests.Unit/Operations/DivisionTests.cs
@@ -0,0 +1,41 @@
+﻿using Calculator.Operations;
+using NUnit.Framework;
+
+namespace Calculator.Tests.Unit.Operations
+{
+	[TestFixture]
+	public class DivisionTests
+	{
+		private Division _operation;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_operation = Division.Instance;
+		}
+
+		[Test]
+		public void HasDivisionSymbol()
+		{
+			// assert
+			Assert.AreEqual('/', _operation.Symbol);
+		}
+
+		[Test]
+		public void HasThreePriority()
+		{
+			// assert
+			Assert.AreEqual(3, _operation.Priority);
+		}
+
+		[Test]
+		public void TestExecute()
+		{
+			// act
+			decimal result = _operation.Execute(10, 3);
+
+			// assert
+			Assert.AreEqual(10.0m / 3, result);
+		}
+	}
+}
