f3a7bee11650a7f81f48b44d54565fabd4fc1584
unknown <victor@govorov-laptop.govorov.ru>
2016-02-24 22:30:58 +0500

    added Multiplication operation

---
+++ src/Calculator/Operations/Multiplication.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculator.Operations
+{
+	public class Multiplication : IArithmeticOperation
+	{
+		public static readonly Multiplication Instance = new Multiplication();
+
+		private Multiplication()
+		{
+			Symbol = '*';
+			Priority = 3;
+		}
+
+		public char Symbol { get; private set; }
+
+		public int Priority { get; private set; }
+
+		public decimal Execute(decimal first, decimal second)
+		{
+			return first * second;
+		}
+	}
+}
---
+++ tests/Calculator.Tests.Unit/Operations/MultiplicationTests.cs
@@ -0,0 +1,41 @@
+﻿using Calculator.Operations;
+using NUnit.Framework;
+
+namespace Calculator.Tests.Unit.Operations
+{
+	[TestFixture]
+	public class MultiplicationTests
+	{
+		private Multiplication _operation;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_operation = Multiplication.Instance;
+		}
+
+		[Test]
+		public void HasMultiplicationSymbol()
+		{
+			// assert
+			Assert.AreEqual('*', _operation.Symbol);
+		}
+
+		[Test]
+		public void HasThreePriority()
+		{
+			// assert
+			Assert.AreEqual(3, _operation.Priority);
+		}
+
+		[Test]
+		public void TestExecute()
+		{
+			// act
+			decimal result = _operation.Execute(10, 3);
+
+			// assert
+			Assert.AreEqual(10 * 3, result);
+		}
+	}
+}
