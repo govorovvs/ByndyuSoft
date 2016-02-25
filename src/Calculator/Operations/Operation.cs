8f2c33b523fb405e8dd92294db818e97242ab4dc
unknown <victor@govorov-laptop.govorov.ru>
2016-02-25 11:46:07 +0500

    added Operation->Resolve

--- src/Calculator/Operations/Operation.cs
+++ src/Calculator/Operations/Operation.cs
@@ -1,7 +1,11 @@
-﻿namespace Calculator.Operations
+﻿using System.Linq;
+
+namespace Calculator.Operations
 {
 	public abstract class Operation : IOperation
 	{
+		private static IOperation[] _operations;
+
 		protected Operation(char symbol, int priority)
 		{
 			Symbol = symbol;
@@ -9,6 +13,31 @@
 		}
 
 		public char Symbol { get; }
+
 		public int Priority { get; }
+
+		public static IOperation Resolve(char symbol)
+		{
+			return Operations.SingleOrDefault(x => x.Symbol == symbol);
+		}
+
+		private static IOperation[] Operations
+		{
+			get
+			{
+				if (_operations == null)
+				{
+					_operations = new IOperation[]
+					{
+						Addition.Instance,
+						Subtraction.Instance,
+						Multiplication.Instance,
+						Division.Instance
+					};
+				}
+
+				return _operations;
+			}
+		}
 	}
 }
---
+++ tests/Calculator.Tests.Unit/Operations/OperationTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Calculator.Operations;
+using NUnit.Framework;
+
+namespace Calculator.Tests.Unit.Operations
+{
+	[TestFixture]
+	public class OperationTests
+	{
+		[TestCase('+', typeof(Addition))]
+		[TestCase('-', typeof(Subtraction))]
+		[TestCase('*', typeof(Multiplication))]
+		[TestCase('/', typeof(Division))]
+		public void TestResolve(char symbol, Type type)
+		{
+			// act
+			var operation = Operation.Resolve(symbol);
+
+			// assert
+			Assert.IsInstanceOf(type, operation);
+		}
+	}
+}
