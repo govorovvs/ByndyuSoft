1c50bc2a188842ba84c78dfbb92ed6bdaf8cd12c
victor.govorov <victor.govorov@engine6.com>
2016-03-18 09:11:11 +0500

    introduced OperationResolver

---
+++ src/Calculator/OperationResolver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Calculator.Operations;
+
+namespace Calculator
+{
+	internal static class OperationResolver
+	{
+		public static IOperation Resolve(char symbol, IEnumerable<IOperation> operations)
+		{
+			return operations.SingleOrDefault(x => x.Symbol == symbol);
+		}
+	}
+}
