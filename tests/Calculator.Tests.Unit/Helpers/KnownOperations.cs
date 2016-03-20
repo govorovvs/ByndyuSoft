6439e66c2ae24613b7a1237293a1878a9242da8d
victor.govorov <victor.govorov@engine6.com>
2016-03-20 23:46:32 +0500

    introduced KnownOperations

---
+++ tests/Calculator.Tests.Unit/Helpers/KnownOperations.cs
@@ -0,0 +1,22 @@
+﻿using Calculator.Operations;
+
+namespace Calculator.Tests.Unit.Helpers
+{
+	internal static class KnownOperations
+	{
+		public static readonly IOperation[] Operations;
+
+		static KnownOperations()
+		{
+			Operations = new IOperation[]
+			{
+				Addition.Instance,
+				Subtraction.Instance,
+				Multiplication.Instance,
+				Division.Instance,
+				LeftBracket.Instance,
+				RightBracket.Instance
+			};
+		}
+	}
+}
