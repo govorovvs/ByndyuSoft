ff2cd149a1aa25b0f2ce3a912f5f85c70e0ef0e7
victor.govorov <victor.govorov@engine6.com>
2016-04-11 00:07:16 +0500

    added UnaryOperation

---
+++ src/Calculator/Operations/UnaryOperation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Calculator.Operations
+{
+	public abstract class UnaryOperation : Operation, IArithmeticOperation
+	{
+		protected UnaryOperation(char symbol, int priority) : base(symbol, priority)
+		{
+		}
+
+		public abstract decimal Execute(decimal argument);
+
+		public void ExecuteOnStack(Stack<decimal> stack)
+		{
+			if (stack.Count == 0)
+				throw new ParseException($"Unexpected operation '{Symbol}'");
+
+			decimal argument = stack.Pop();
+
+			decimal result = Execute(argument);
+
+			stack.Push(result);
+		}
+	}
+}
---
+++ tests/Calculator.Tests.Unit/Operations/UnaryOperationTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Calculator.Operations;
+using NUnit.Framework;
+
+namespace Calculator.Tests.Unit.Operations
+{
+	[TestFixture]
+	public class UnaryOperationTests
+	{
+		[Test]
+		public void ExecuteOnStack_Gets_Arg_From_Stack_And_Puts_Result()
+		{
+			UnaryOperation binaryOp =
+				new FakeUnaryOperation();
+
+			var stack = new Stack<decimal>();
+			stack.Push(10);
+
+			binaryOp.ExecuteOnStack(stack);
+
+			Assert.That(-10, Is.EqualTo(stack.Single()));
+		}
+
+		private class FakeUnaryOperation : UnaryOperation
+		{
+			public FakeUnaryOperation() : base('-', 3)
+			{
+			}
+
+			public override decimal Execute(decimal arg)
+			{
+				return -arg;
+			}
+		}
+	}
+}
