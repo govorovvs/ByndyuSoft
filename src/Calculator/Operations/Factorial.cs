e67427a073eb5a346fa743c6e16ab3d48baac64f
victor.govorov <victor.govorov@engine6.com>
2016-04-11 00:25:12 +0500

    added Factorial operation

---
+++ src/Calculator/Operations/Factorial.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculator.Operations
+{
+	public class Factorial : UnaryOperation
+	{
+		public static readonly Factorial Instance = new Factorial();
+
+		private Factorial() : base('!', 4)
+		{
+		}
+
+		public override decimal Execute(decimal argument)
+		{
+			decimal result = 1;
+			for (int i = 1; i <= argument; i++)
+			{
+				result *= i;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ tests/Calculator.Tests.Unit/Operations/FactorialTests.cs
@@ -0,0 +1,41 @@
+﻿using Calculator.Operations;
+using NUnit.Framework;
+
+namespace Calculator.Tests.Unit.Operations
+{
+	[TestFixture]
+	public class FactorialTests
+	{
+		private Factorial _operation;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_operation = Factorial.Instance;
+		}
+
+		[Test]
+		public void HasExclamationMarkSymbol()
+		{
+			// assert
+			Assert.AreEqual('!', _operation.Symbol);
+		}
+
+		[Test]
+		public void HasFourPriority()
+		{
+			// assert
+			Assert.AreEqual(4, _operation.Priority);
+		}
+
+		[Test]
+		public void TestExecute()
+		{
+			// act
+			decimal result = _operation.Execute(5);
+
+			// assert
+			Assert.AreEqual(1*2*3*4*5, result);
+		}
+	}
+}
